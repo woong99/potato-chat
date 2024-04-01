ead41d2a437ccb882a79afd9619a32b679190fda
woong99 <wg.jeong@springworks.co.kr>
2024-04-01 21:16:26 +0900

    init project

---
+++ src/main/java/potatowoong/potatochat/PotatoChatApplication.java
@@ -0,0 +1,13 @@
+package potatowoong.potatochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/potatowoong/potatochat/PotatoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package potatowoong.potatochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
