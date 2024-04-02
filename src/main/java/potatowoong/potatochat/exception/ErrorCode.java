db25258b9302f34d65ca0ac0fcc3d0028f61c83e
woong99 <wg.jeong@springworks.co.kr>
2024-04-02 22:20:46 +0900

    feat: 권한 관련 공통 예외처리 코드 추가
    1. UNAUTHORIZED,
    2. FORBIDDEN

--- src/main/java/potatowoong/potatochat/exception/ErrorCode.java
+++ src/main/java/potatowoong/potatochat/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
     EXIST_ID(HttpStatus.BAD_REQUEST, "A0001", "이미 존재하는 아이디입니다."),
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "A0002", "비밀번호가 일치하지 않습니다."),
-    EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "A0003", "이미 존재하는 닉네임입니다.");
+    EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "A0003", "이미 존재하는 닉네임입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A0004", "인증되지 않은 사용자입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "A0005", "권한이 없습니다.");
 
     private final HttpStatus httpStatus;
 
