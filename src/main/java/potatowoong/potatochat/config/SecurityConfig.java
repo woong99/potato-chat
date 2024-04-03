1ef2cd97dc3cb2ab75d3712cf8383933368db24e
woong99 <wg.jeong@springworks.co.kr>
2024-04-03 16:27:17 +0900

    [back] feat: spring security cors 설정 추가

--- src/main/java/potatowoong/potatochat/config/SecurityConfig.java
+++ src/main/java/potatowoong/potatochat/config/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import potatowoong.potatochat.jwt.component.JwtAccessDeniedHandler;
 import potatowoong.potatochat.jwt.component.JwtAuthenticationEntryPoint;
 import potatowoong.potatochat.jwt.component.JwtTokenProvider;
@@ -21,16 +24,13 @@ import potatowoong.potatochat.jwt.filter.JwtFilter;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-
-    private final String[] WHITE_LIST = {
+    private static final String[] WHITE_LIST = {
         "/api/member/sign-up",
         "/api/member/login"
     };
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -41,6 +41,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfiguration()))
             .exceptionHandling(exceptionHandling -> exceptionHandling
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler))
@@ -54,5 +55,18 @@ public class SecurityConfig {
             .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
