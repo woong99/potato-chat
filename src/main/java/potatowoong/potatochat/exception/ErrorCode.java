bd3861ea742b385f6131aa8c464cb344dd1ea2d5
woong99 <wg.jeong@springworks.co.kr>
2024-04-07 20:40:23 +0900

    [back] feat: 공통 예외 코드 추가
    1. BAD_REQUEST

--- src/main/java/potatowoong/potatochat/exception/ErrorCode.java
+++ src/main/java/potatowoong/potatochat/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "A0002", "비밀번호가 일치하지 않습니다."),
     EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "A0003", "이미 존재하는 닉네임입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A0004", "인증되지 않은 사용자입니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "A0005", "권한이 없습니다.");
+    FORBIDDEN(HttpStatus.FORBIDDEN, "A0005", "권한이 없습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "A0006", "잘못된 요청입니다.");
 
     private final HttpStatus httpStatus;
 
